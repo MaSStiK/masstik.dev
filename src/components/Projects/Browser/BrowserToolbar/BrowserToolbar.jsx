540d2d1e0e256d38951e561aea4f97e12154d78a
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-09-22 01:42:00 +0200

    feat: add goBack function in browser toolbar

--- src/components/Projects/Browser/BrowserToolbar/BrowserToolbar.jsx
+++ src/components/Projects/Browser/BrowserToolbar/BrowserToolbar.jsx
@@ -2,15 +2,38 @@ import { ChevronLeft, RefreshCw, Search } from "lucide-react"
 import { LockIcon } from "@/icons"
 import useLocale from "@/hooks/useLocale"
 import useCategoryFilter from "@/hooks/useCategoryFilter"
+import useActiveProject from "@/hooks/useActiveProject"
 
 import "./BrowserToolbar.css"
 
 export default function BrowserToolbar() {
-    const [categoryFilter] = useCategoryFilter()
+    const [categoryFilter, setCategoryFilter] = useCategoryFilter()
+    const [activeProject, setActiveProject] = useActiveProject()
     const locale = useLocale()
 
     // Отображение выбранной категории
-    const categoryQuery = categoryFilter !== "all" && `?category=${categoryFilter}`
+    const categoryQuery = categoryFilter !== "all"
+        ? `?category=${categoryFilter}`
+        : ""
+
+    // Отображаем проект, если он выбран, иначе категорию
+    const projectPath = activeProject
+        ? `/${activeProject.slug}`
+        : categoryQuery
+
+    // Кнопка Назад
+    function goBack() {
+        // Если открыт проект - закрываем его и возвращаемся в выбранную категорию
+        if (activeProject) {
+            setActiveProject(null)
+            return
+        }
+
+        // Если выбрана категория - сбрасываем её и возвращаемся ко всем проектам
+        if (categoryFilter !== "all") {
+            setCategoryFilter("all")
+        }
+    }
 
     return (
         <div className="browser__toolbar">
@@ -24,6 +47,7 @@ export default function BrowserToolbar() {
                 <button
                     className="button-transition flex-center browser__toolbar-button"
                     aria-label={locale.projects.toolbar.back}
+                    onClick={goBack}
                 >
                     <ChevronLeft size={16} />
                 </button>
@@ -35,7 +59,7 @@ export default function BrowserToolbar() {
                 </button>
                 <div className="browser__bar browser__address-bar">
                     <LockIcon size={16} />
-                    <span>masstik.dev/projects{categoryQuery}</span>
+                    <span>masstik.dev/projects{projectPath}</span>
                 </div>
             </div>
 
